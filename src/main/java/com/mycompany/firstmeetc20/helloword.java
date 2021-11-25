efb053d7982574961515733b8bd4c3d09e2e1b5a
Wardani <dediduank@gmail.com>
2021-11-25 17:31:31 +0700

    Create Project

---
+++ src/main/java/com/mycompany/firstmeetc20/helloword.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.firstmeetc20;
+
+/**
+ *
+ * @author Dedi Wardani
+ */
+public class helloword {
+    public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
+}
